2a1826d4fd19f4bd62957ebe187b2528e1583ae9
Javier <jevans@avvo.com>
2017-11-16 15:36:49 -0800

    finish of day two

--- meeting_2/bowling.ex
+++ meeting_2/bowling.ex
@@ -1,3 +1,9 @@
 defmodule Bowling do
+  def calculate_score(scores), do: calculate_score(scores, 0)
 
+  def calculate_score([], score), do: score
+
+  def calculate_score([[first_roll, second_roll] | rest], score) do
+    calculate_score(rest, score + first_roll + second_roll)
+  end
 end
